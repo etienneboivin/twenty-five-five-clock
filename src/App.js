5737b4f76c5eb1e1f7588584c5d922b937ed0bb5
atms <etiennedtboivin@gmail.com>
2022-06-30 16:43:04 -0700

    changes didn't stick?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TimerHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+    )
+  }
 }
 
-export default App;
+export default TimerHeader;
