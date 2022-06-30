53623ae1782a23ce969af53b62637c9269fad809
atms <etiennedtboivin@gmail.com>
2022-06-30 16:37:15 -0700

    small changes to create-react-app files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TimerHeader from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TimerHeader />
   </React.StrictMode>
 );
 
