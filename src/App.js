91e062ab92a8d0f5b10b525ae87ff1e8802fb664
atms <etiennedtboivin@gmail.com>
2022-07-01 09:02:31 -0700

    Using fontawesomeicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core"
 import {faArrowDown, faArrowUp} from "@fortawesome/free-solid-svg-icons"
 import './App.css';
 
