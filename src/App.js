4618eb20c263853007b94940c9b478d4dfc959fc
atms <etiennedtboivin@gmail.com>
2022-07-01 08:31:40 -0700

    Trying out icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './logo.svg';
+import { IconName } from "react-icons/fa";
 import './App.css';
 
-class TimerHeader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <div id="break-label">Break Length</div>
-        <div id="session-label">Session Length</div>
-      </>
-    )
-  }
+function TimerHeader() {
+  // TODO State hook goes here
+  return (
+    <>
+      <i class="fa fa-arrow-down"></i>
+      <div id="break-label">Break Length</div>
+      <i class="fa fa-arrow-up"></i>
+      <i class="fa fa-arrow-down"></i>
+      <div id="session-label">Session Length</div>
+      <i class="fa fa-arrow-up"></i>
+    </>
+  )
 }
+
 
 export default TimerHeader;
