8ffd9d7f3a5c673abbedad1902d99dd1868e26ea
atms <etiennedtboivin@gmail.com>
2022-07-01 08:54:45 -0700

    Using fontawesomeicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowDown, faArrowUp} from "@fortawesome/free-solid-svg-icons"
 import './App.css';
+
+library.add(faArrowDown, faArrowUp);
 
 function TimerHeader() {
   // TODO State hook goes here
