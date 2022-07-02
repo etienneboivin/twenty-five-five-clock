53b885ec8011a6f0f7c67260f8aa941c6341879c
atms <etiennedtboivin@gmail.com>
2022-07-01 19:53:10 -0700

    TimerLengthControl and Timer classes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core"
@@ -7,19 +7,65 @@ import './App.css';
 
 library.add(faArrowDown, faArrowUp);
 
-function TimerHeader() {
-  // TODO State hook goes here
-  return (
-    <>
-      <FontAwesomeIcon icon="fa-arrow-down" />
-      <div id="break-label">Break Length</div>
-      <FontAwesomeIcon icon="fa-arrow-up" />
-      <FontAwesomeIcon icon="fa-arrow-down" />
-      <div id="session-label">Session Length</div>
-      <FontAwesomeIcon icon="fa-arrow-up" />
-    </>
-  )
+class TimerLengthControl extends React.Component {
+  // Two buttons & a label. Two needed in the timer, one for session and one for breaks.
+  // props to be passed: Label (Title), initial length, button needs value
+  // buttons should not work if Timer is running
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.length
+    }
+  }
+
+  onClick = () => {
+    if(this.state.sign === "+" && this.state.count !== 60) {
+      setState({count: count + 1});
+    } else if (this.state.sign === "-" && this.state.count !== 1) {
+      setState({count: count - 1});
+    }
+  }
+
+  setBreakLength = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="break-label">{this.props.title}</div>
+        <button sign="-" onClick={this.props.onClick}
+          <FontAwesomeIcon icon="fa-arrow-down" />
+        </button>
+        {this.state.count}
+        <button sign="+" onClick={() => this.props.onClick}>
+          <FontAwesomeIcon icon="fa-arrow-up" />
+        </button>
+      </div>
+    )
+  }
 }
 
+class Timer extends React.Component {
+  // Contains title, two TimerLengthControl, and actual session/break display, pause/start/reset button
+  // will be passed timer length, whether on break or on session
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
 
-export default TimerHeader;
+  render () {
+      return (
+        <>
+          <div className="main-title">25 + 5 Clock</div>
+          <TimerLengthControl
+            title="Break Length"
+            length=5 />
+          <TimerLengthControl
+            title="Session Length"
+            length=25 />
+        </>
+    )
+  }
+}
